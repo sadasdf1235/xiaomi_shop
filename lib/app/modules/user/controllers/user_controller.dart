f6175de8a3088085f6598d9f5c40d8118286b6cb
liuding <116145942+sadasdf1235@users.noreply.github.com>
2024-08-19 22:28:34 +0800

    userInfo

--- lib/app/modules/user/controllers/user_controller.dart
+++ lib/app/modules/user/controllers/user_controller.dart
@@ -1,23 +1,39 @@
 import 'package:get/get.dart';
+import '../../../models/user/user_model.dart';
+import '../../../services/userServices.dart';
 
 class UserController extends GetxController {
-  //TODO: Implement UserController
-
-  final count = 0.obs;
+  RxBool isLogin=false.obs;
+  // RxList userList=[].obs;
+  var userInfo=UserModel().obs;
   @override
   void onInit() {
+    print("userInfo onInit");
     super.onInit();
+    getUserInfo();
   }
 
-  @override
-  void onReady() {
-    super.onReady();
-  }
 
   @override
   void onClose() {
     super.onClose();
   }
+  getUserInfo() async{
+    var tempLoginState=await UserServices.getUserLoginState();
+    isLogin.value=tempLoginState;
+    var tempList=await UserServices.getUserInfo();
+    if(tempList.isNotEmpty){     
+      userInfo.value=UserModel.fromJson(tempList[0]);
+      update();
+    }
+  }
 
-  void increment() => count.value++;
+  loginOut(){
+    UserServices.loginOut();
+     isLogin.value=false;
+     //把信息值为空
+     userInfo.value=UserModel();
+     update();
+  }
+
 }
